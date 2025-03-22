e7ebc23cca4b598da5d7e919f89f8826a123b9e6
d4m14n-gh <damiantrowski.c@gmail.com>
2025-03-22 02:07:56 +0000

    Quick fix

--- src/tasks/task.py
+++ src/tasks/task.py
@@ -18,11 +18,8 @@ class Task(Base):
     id = Column(String, primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     file_path = Column(String, index=True)
 
-class TaskCreate(BaseModel):
+class TaskDTO(BaseModel):
     id: str
-    file_path: str
-
-class TaskUpdate(BaseModel):
     file_path: str
 
 app = FastAPI()
@@ -37,38 +34,39 @@ def get_db():
         db.close()
 
 
-@app.post("/tasks/", response_model=TaskCreate)
+@app.post("/tasks/", response_model=TaskDTO)
 def create_task(db: Session = Depends(get_db), files: list[UploadFile] = File(...)):
-    task_files_path = f"/data/tasks/{str(uuid.uuid4())}"
+    task_id = str(uuid.uuid4())
+    task_host_path = f"/home/damian/Projekty/Projekt_Inzynierski-2025/stos-files/tasks/{task_id}"
+    task_path = f"/data/tasks/{task_id}"
     
-    os.makedirs(f"{task_files_path}", exist_ok=True)
-    os.makedirs(f"{task_files_path}/in", exist_ok=True)
-    os.makedirs(f"{task_files_path}/out", exist_ok=True)
-    os.chmod(f"{task_files_path}", 0o777)
-    os.chmod(f"{task_files_path}/in", 0o777)
-    os.chmod(f"{task_files_path}/out", 0o777)
+    os.makedirs(f"{task_path}", exist_ok=True)
+    os.makedirs(f"{task_path}/in", exist_ok=True)
+    os.makedirs(f"{task_path}/out", exist_ok=True)
+    os.chmod(f"{task_path}", 0o777)
+    os.chmod(f"{task_path}/in", 0o777)
+    os.chmod(f"{task_path}/out", 0o777)
     
-    for in_file in files:
-        file_path = os.path.join(f"{task_files_path}/in", in_file.filename)
-        if in_file.filename.endswith(".out"):
-            file_path = os.path.join(f"{task_files_path}/out", in_file.filename)
+    for file in files:
+        file_path = os.path.join(f"{task_path}/in", file.filename)
+        if file.filename.endswith(".out"):
+            file_path = os.path.join(f"{task_path}/out", file.filename)
         with open(file_path, "wb") as f:
-            f.write(in_file.file.read())
+            f.write(file.file.read())
 
-    db_task = Task(id=str(uuid.uuid4()), file_path=task_files_path)
+    db_task = Task(id=task_id, file_path=task_host_path)
     db.add(db_task)
     db.commit()
-    db.refresh(db_task)
     return db_task
 
 
-@app.get("/tasks/", response_model=List[TaskCreate])
+@app.get("/tasks/", response_model=List[TaskDTO])
 def read_tasks(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     tasks = db.query(Task).offset(skip).limit(limit).all()
     return tasks
 
 
-@app.get("/tasks/{task_id}", response_model=TaskCreate)
+@app.get("/tasks/{task_id}", response_model=TaskDTO)
 def read_task(task_id: str, db: Session = Depends(get_db)):
     task = db.query(Task).filter(Task.id == task_id).first()
     if task is None:
@@ -76,18 +74,7 @@ def read_task(task_id: str, db: Session = Depends(get_db)):
     return task
 
 
-@app.put("/tasks/{task_id}", response_model=TaskCreate)
-def update_task(task_id: str, task: TaskUpdate, db: Session = Depends(get_db)):
-    db_task = db.query(Task).filter(Task.id == task_id).first()
-    if db_task is None:
-        raise HTTPException(status_code=404, detail="Task not found")
-    db_task.file_path = task.file_path
-    db.commit()
-    db.refresh(db_task)
-    return db_task
-
-
-@app.delete("/tasks/{task_id}", response_model=TaskCreate)
+@app.delete("/tasks/{task_id}", response_model=TaskDTO)
 def delete_task(task_id: str, db: Session = Depends(get_db)):
     db_task = db.query(Task).filter(Task.id == task_id).first()
     if db_task is None:
